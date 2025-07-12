3ae96dcec97d92f37147cbcbcebf76ac2e903a2c
Samuel Fernandes <samuelfernandesfilho2007@gmail.com>
2025-07-12 15:53:56 -0300

    Adicionando o login_required | Mensagens Flash | Mudança nos botões (entrar e sair)

--- app/routes/home_bp.py
+++ app/routes/home_bp.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template, redirect, url_for
+from flask_login import login_required
 
 home = Blueprint('home', __name__)
 
 @home.route('/')
+@login_required
 def homepage():
     return render_template('home.html')
