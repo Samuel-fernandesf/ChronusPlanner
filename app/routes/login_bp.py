fc75351ac4a0ea592c25346ad18c81c6a35fcee1
Samuel Fernandes <samuelfernandesfilho2007@gmail.com>
2025-07-12 15:50:15 -0300

    Criando função de login e logout | Correção de bugs

--- app/routes/login_bp.py
+++ app/routes/login_bp.py
@@ -1,51 +1,33 @@
-from flask import render_template, request, redirect, url_for, flash, Blueprint, current_app
-from flask_login import login_user
+from flask import Blueprint, render_template, request, redirect, url_for, flash, current_app
+from flask_login import login_user, logout_user, current_user
+from app.models.user import User
 from app.DAO.userDAO import UserDAO
 
 login = Blueprint('login', __name__)
-
-from flask_login import login_user  # Agora essa função será usada corretamente
+userDAO = UserDAO()
 
 @login.route("/", methods=["GET", "POST"])
-def login_view():  # 👈 nome diferente de login_user!
-    if request.method == "POST":
-        email = request.form["email"]
-        password = request.form["password"]
-        user = UserDAO.get_user_by_email(email)
+def login_usuario():
+    
+    if current_user.is_authenticated:
+        return redirect(url_for('home.homepage'))
 
-        if user and user.check_password(password):
+    if request.method == "POST":
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+
+        user = userDAO.buscar_por_email(email)
+
+        if user and user.check_password(senha):
             login_user(user) 
             flash("Login bem-sucedido!", "success")
             return redirect(url_for("home.homepage"))
         else:
-            flash("Credenciais inválidas.", "danger")
-
+            flash("Usuário ou senha inválidos!", "danger")
     return render_template("login.html")
 
-
-@login.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        username = request.form["nome"]
-        email = request.form["email"]
-        password = request.form["password"]
-        confirm = request.form["confirm_password"]
-
-        if password != confirm:
-            flash("As senhas não coincidem.", "warning")
-            return redirect(url_for("login.register"))
-
-        # Verifica se já existe usuário
-        if UserDAO.get_user_by_email(email):
-            flash("Este e-mail já está cadastrado.", "danger")
-            return redirect(url_for("login.register"))
-
-        UserDAO.create_user(username, email, password)
-        flash("Cadastro realizado com sucesso! Faça o login.", "success")
-        return redirect('/')
-
-    return render_template("register.html")
-
-
-
-
+@login.route('/logout')
+def logout_usuario():
+    logout_user()
+    flash(f'Você fez o logout!', category='info')
+    return redirect(url_for('login.login_usuario'))
