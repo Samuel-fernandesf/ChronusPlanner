cec7d2d2ea4522e5954c6f6c7d3a23a8da1d7cd4
Samuel Fernandes <samuelfernandesfilho2007@gmail.com>
2025-07-14 17:35:10 -0300

    Implementando o formulário e lógica de login e cadastro

--- app/routes/cadastro_bp.py
+++ app/routes/cadastro_bp.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
-from flask_login import login_user, logout_user, current_user
+from app.forms import Cadastro_Formulario
 from app.models.user import User
 from app.DAO.userDAO import UserDAO
+from flask_login import login_user, logout_user, current_user
 
 cadastro = Blueprint('cadastro', __name__)
 userDAO = UserDAO()
@@ -9,19 +10,12 @@ userDAO = UserDAO()
 @cadastro.route('/',  methods=["GET", "POST"])
 def cadastro_usuario():
 
-    if request.method == "POST":
-        email = request.form.get('email')
-        senha = request.form.get('senha')
-        confirm_senha = request.form.get("confirm_senha")
-        nome = request.form.get('nome')
-
-        if senha != confirm_senha:
-            flash("Senhas diferentes. Tente novamente.", "warning")
-            return redirect(url_for("cadastro.cadastro_usuario"))
-
-        if userDAO.buscar_por_email(email):
-            flash("E-mail já cadastrado, tente outro.", "danger")
-            return redirect(url_for("cadastro.cadastro_usuario"))
+    form = Cadastro_Formulario()
+    if form.validate_on_submit():
+        email = form.email.data
+        senha = form.senha.data
+        confirm_senha = form.confirm_senha.data
+        nome = form.nome.data
         
         user = User(nome=nome, email=email)
         user.set_password(senha)
@@ -31,4 +25,4 @@ def cadastro_usuario():
         flash(f'Conta criada com sucesso para {nome}!', 'success' )
         return redirect(url_for('home.homepage'))
         
-    return render_template('cadastro.html')
+    return render_template('cadastro.html', form=form)
--- app/routes/login_bp.py
+++ app/routes/login_bp.py
@@ -1,30 +1,32 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, current_app
-from flask_login import login_user, logout_user, current_user
+from app.forms import Login_Formulario
 from app.models.user import User
 from app.DAO.userDAO import UserDAO
+from flask_login import login_user, logout_user, current_user
 
 login = Blueprint('login', __name__)
 userDAO = UserDAO()
 
 @login.route("/", methods=["GET", "POST"])
 def login_usuario():
-    
+
     if current_user.is_authenticated:
         return redirect(url_for('home.homepage'))
 
-    if request.method == "POST":
-        email = request.form.get('email')
-        senha = request.form.get('senha')
+    form = Login_Formulario()
+    if form.validate_on_submit():
+        email = form.email.data
+        senha = form.senha.data
 
         user = userDAO.buscar_por_email(email)
 
         if user and user.check_password(senha):
             login_user(user) 
-            flash("Login bem-sucedido!", "success")
+            flash(f"Seja bem-vindo {user.nome}!", "success")
             return redirect(url_for("home.homepage"))
         else:
             flash("Usuário ou senha inválidos!", "danger")
-    return render_template("login.html")
+    return render_template("login.html", form=form)
 
 @login.route('/logout')
 def logout_usuario():
